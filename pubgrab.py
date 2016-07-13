a09ae17df8f4a135534132d20df48dbecf965fbe
Jon Olav Vik <jonovik@gmail.com>
2016-07-13 11:36:44 +0200

    Process multiple authors, replacing the previous main script.
    Add deduplicate().
    Add command-line debug option. (Todo: Add author list.)
    
    Sample output:
    
    $ pubgrab.py --debug
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTPS connection (1): api.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /v1/persons?name=Jon+Olav+Vik HTTP/1.1" 200 147
    DEBUG:root:Getting URL: http://www.cristin.no/ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=22311
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTP connection (1): www.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=22311 HTTP/1.1" 200 None
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTPS connection (1): api.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /v1/persons?name=Dag+Inge+V%C3%A5ge HTTP/1.1" 200 147
    DEBUG:root:Getting URL: http://www.cristin.no/ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=2529
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTP connection (1): www.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=2529 HTTP/1.1" 200 None
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTPS connection (1): api.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /v1/persons?name=Sigbj%C3%B8rn+Lien HTTP/1.1" 200 147
    DEBUG:root:Getting URL: http://www.cristin.no/ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=7051
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTP connection (1): www.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=7051 HTTP/1.1" 200 None
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTPS connection (1): api.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /v1/persons?name=Arne+Bj%C3%B8rke+Gjuvsland HTTP/1.1" 200 155
    DEBUG:root:Getting URL: http://www.cristin.no/ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=7059
    INFO:requests.packages.urllib3.connectionpool:Starting new HTTP connection (1): www.cristin.no
    DEBUG:requests.packages.urllib3.connectionpool:"GET /ws/hentVarbeiderPerson?format=json&hovedkategori=TIDSSKRIFTPUBL&til=2015&fra=2003&lopenr=7059 HTTP/1.1" 200 None
    DEBUG:root:4 authors with a total of 178 publications
    DEBUG:root:166 unique publications

--- pubgrab.py
+++ pubgrab.py
@@ -11,7 +11,10 @@ Old API docs: http://www.cristin.no/cristin/superbrukeropplaering/ws-dokumentasj
 Old XML schema: http://www.cristin.no/techdoc/xsd/resultater/1.0/
 Info on transition from old to new API: http://www.cristin.no/om/aktuelt/aktuelle-saker/2016/api-lansering.html
 """
+
+import argparse
 import operator
+import itertools
 import requests
 import logging
 from urllib.parse import urlencode
@@ -168,6 +171,20 @@ def format_author(a):
     initials = "".join(i[0] for i in given_names)
     return a["etternavn"] + " " + initials
 
+def deduplicate(pubs):
+    """
+    Remove duplicate publications from list.
+
+    >>> abg = pubs_by("Arne Gjuvsland", fra=2010, til=2014)
+    >>> jov = pubs_by("Jon Olav Vik", fra=2010, til=2014)
+    >>> len(abg)
+    17
+    >>> len(jov)
+    10
+    >>> len(deduplicate([abg, jov]))
+    19
+    """
+    return {p["id"]: p for p in itertools.chain(*pubs)}.values()
 
 def citation(pub, html=False):
     """
@@ -231,112 +248,20 @@ def bibliography_author(author, *args, **kwargs):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compile HTML bibliography from CRISTIN for list of authors.")
+    parser.add_argument("-d", "--debug", help="log debug messages", action="store_true")
+    args = parser.parse_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
 
-    with open("test.html", "w") as f:
-        f.write(bibliography_author("Jon Olav Vik"))
-
-    import sys
-    sys.exit(0)
-
-    # set up which user to search for will be passed to an ARGV in the future
-    # To be added later start year and end year
-    # start_year = 2015 end_year = 2015
-    name_of_user = "Jon Olav Vik"
-    # name_of_user = "Dag Inge Våge"
-    # name_of_user = "Sigbjørn Lien"
-    # name_of_user = "Arne Bjørke Gjuvsland"
-
-    # Initiate the url using pubs_by
-    data = pubs_by(name_of_user)
-
-    # Create a list containing all information for printing html code
-    html_codes_all = []
-
-    # Sort data according to year of publication, newest first
-    # noinspection PyShadowingNames
-    data_sort = sorted((data['forskningsresultat']), key=lambda k: k['fellesdata']['ar'], reverse=True)
-    # Start to iterate through all the "forskningsresultat" list
-    for i in data_sort:
-        # Create a string for name for each iteration
-        authors = ''
-        # Iterate through all persons in the project and create a name list
-        # Some instances are lists when multiple authors
-        if type(i['fellesdata']['person']) == list:
-            for k in i['fellesdata']['person']:
-                # Convert first names to uppercase letters only
-                Short_firstname = k['fornavn']
-                Uppercase_firstname = ''.join(c for c in Short_firstname if c.isupper())
-                # Append all the names to a string
-                authors += ((k['etternavn'] + ' ' + Uppercase_firstname) + ', ')
-            # Remove a comma and a white space at the end of strings
-            authors = authors[:-2]
-
-        # For instances when there is a single author
-        elif type(i['fellesdata']['person']) == dict:
-            Short_firstname = i['fellesdata']['person']['fornavn']
-            Uppercase_firstname = ''.join(c for c in Short_firstname if c.isupper())
-            # Append name to a string
-            authors = i['fellesdata']['person']['etternavn'] + ' ' + Uppercase_firstname
-
-        # Iterate through all keys in the categories.
-        for key, value in (i['kategoridata']).items():
-            # Start processing data based on known keys. This will be expanded later
-            if key == 'tidsskriftsartikkel':
-                tittel = i['fellesdata']['tittel']
-                year = i['fellesdata']['ar']
-                journal = '<em>' + i['kategoridata']['tidsskriftsartikkel']['tidsskrift']['navn'] + '</em>'
-
-                try:
-                    articlenr = i['kategoridata']['tidsskriftsartikkel']['artikkelnr']
-                except KeyError:
-                    articlenr = ''
-
-                try:
-                    volum = '<strong>(' + i['kategoridata']['tidsskriftsartikkel']['volum'] + ')</strong>'
-                except KeyError:
-                    volum = ''
-
-                try:
-                    DOI = 'http://dx.doi.org/' + i['kategoridata']['tidsskriftsartikkel']['doi']
-                except KeyError:
-                    DOI = ''
-
-                HTML_string = ('<p> ' + authors + ' (' + year + '). ' + tittel + ' ' + journal + ' ' + volum +
-                               ' ' + articlenr + ' ' + DOI + '</p>')
-                html_codes_all.append(HTML_string)
-
-                # Currently only working for Articles, codes below can be used to add more items.
-                # elif key == ( 'foredragPoster' ):
-                #     tittel = i['fellesdata']['tittel']
-                #     year = i['fellesdata']['ar']
-                #     #journal = '<em>'+i['kategoridata'][key]['navn']+'</em>' Wrokshop
-                # elif key == 'bokRapport':
-                #     tittel = '<em>'+i['fellesdata']['tittel']+'</em>'
-                #     year = '<strong>'+i['fellesdata']['ar']+'</strong>'
-                #
-                # elif key == 'bokRapportDel':
-                #     tittel = '<em>'+i['fellesdata']['tittel']+'</em>'
-                #     year = '<strong>'+i['fellesdata']['ar']+'</strong>'
-
-    html_str_start = """
-    <!DOCTYPE html>
-    <HTML>
-        <head>
-            <meta charset="utf-8">
-        </head>
-
-        <body>
-            <h1>References</h1>
+    authors = """
+    Jon Olav Vik
+    Dag Inge Våge
+    Sigbjørn Lien
+    Arne Bjørke Gjuvsland
     """
-
-    html_str_end = """
-        </body>
-    </HTML>
-    """
-
-    filename = name_of_user + '.html'
-    with open(filename, 'w+', encoding="utf-8") as myFile:
-        myFile.write(html_str_start)
-        for i in html_codes_all:
-            myFile.write('\t\t' + i + '\n')
-        myFile.write(html_str_end)
+    authors = [a.strip() for a in authors.strip().split("\n")]
+    pubs = [pubs_by(a, fra=2003, til=2015) for a in authors]
+    logging.debug("%s authors with a total of %s publications", len(authors), sum(len(p) for p in pubs))
+    pubs = deduplicate(pubs)
+    logging.debug("%s unique publications", len(pubs))
